4959aa7481bfeeff6ab347fd590114f7439f99db
zhiyisoh <zhiyi.soh.2021@scis.smu.edu.sg>
2022-09-12 14:26:02 +0800

    Added FrontEnd: first commit
    Using Vue for FrontEnd

---
+++ FrontEnd/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import StartView from '../views/Start.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'start',
+      component: StartView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
+    }
+  ]
+})
+
+export default router
