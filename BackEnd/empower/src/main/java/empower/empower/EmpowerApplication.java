087951a5f7076e5bdb52bcaf766ea4e88035136a
zhiyisoh <zhiyi.soh.2021@scis.smu.edu.sg>
2022-09-13 09:48:02 +0800

    Adding BackEnd: First commit

---
+++ BackEnd/empower/src/main/java/empower/empower/EmpowerApplication.java
@@ -0,0 +1,13 @@
+package empower.empower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerApplication.class, args);
+	}
+
+}
---
+++ BackEnd/empower/src/test/java/empower/empower/EmpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package empower.empower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
