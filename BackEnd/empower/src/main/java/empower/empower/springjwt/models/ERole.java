742750817495eedaa0b0f775fb2461f29c48d05a
cheese <kwekzhiyi2511@gmail.com>
2022-09-26 08:51:03 +0800

    add name of role admin

--- BackEnd/empower/src/main/java/empower/empower/springjwt/models/ERole.java
+++ BackEnd/empower/src/main/java/empower/empower/springjwt/models/ERole.java
@@ -2,6 +2,7 @@ package empower.empower.springjwt.models;
 
 //basically these are the roles permanent names, can add more if we ever wanna add lmao
 public enum ERole {
-    ROLE_USER
+    ROLE_USER,
+    ROLE_ADMIN
 }
     
