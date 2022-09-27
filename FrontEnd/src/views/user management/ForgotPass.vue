491a94cb37038d45fbec0b6725a7549de485ddbf
Jess <jess.toh.2021@scis.smu.edu.sg>
2022-09-27 15:30:58 +0800

    wip forgot password

--- FrontEnd/src/views/user management/ForgotPass.vue
+++ FrontEnd/src/views/user management/ForgotPass.vue
@@ -1,16 +1,47 @@
+
 <template>
     <div class="verification">
         <div class="veri-white" style="text-align:center">
          <h1 class="title">Forgot Password</h1>
          <img src="/src/assets/leave-line.svg" alt="leaves" class="leaves-line">
          <h4>Please enter your email to reset password</h4>
-            <div class="form-group">
-                <input type="email" class="form-control" style="width: 75%; display: inline; margin-right: 15px" placeholder="Enter email">
+
+          <div class="form-group">
+              
+            <form @submit.prevent="handleSubmit"> 
+                <input type="email" class="form-control" v-model="email" style="width: 75%; display: inline; margin-right: 15px" placeholder="Enter email">
                 <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+    name:'forgotpassword',
+    data(){
+      return{
+        email: ''
+      }
+
+    }
+    ,
+    
+    // methods: {
+    //   async handleSubmit(){
+    //     const response = await axios.post('forgotpassword',{
+    //       email = this.email
+    //     });
+
+    //     console.log(response);
+    //   }
+    // }
+
+  }
+</script>
 
   <style>
     .verification {
