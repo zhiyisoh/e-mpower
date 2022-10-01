59bf1b92725a81af92c695202c72ed13130b1b49
Jesleen <jesleent.2021@scis.smu.edu.sg>
2022-10-01 15:21:02 +0800

    card test

--- FrontEnd/src/views/logging/Log.vue
+++ FrontEnd/src/views/logging/Log.vue
@@ -1,11 +1,39 @@
+<script setup>
+    import Footer from "../components/Footer.vue";
+    </script>
+
 <template>
     <h1>My Log </h1>
-    <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">Bottle</h5>
+
+    <div class="past-logs">
+        <div class="row row-cols-1 row-cols-md-3 g-4 logs">
+
+        <div class="log-entry">
+            <div class="card h-100">
+                <div class="card-body">
+                <h5 class="card-title">Bottle</h5>
+                </div>
             </div>
-          </div>
         </div>
 
+        </div>
+
+    </div>
+
 </template>
+
+<style>
+.past-logs {
+  padding: 7% 0%;
+}
+
+.logs {
+  margin: 30px 0;
+}
+
+.card {
+  background-color: #CEE5D0;
+  border: 0;
+}
+
+</style>
