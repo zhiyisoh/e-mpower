086bf67171649f666711167c87c86ed2a3bd1356
cheese <kwekzhiyi2511@gmail.com>
2022-10-02 17:59:13 +0800

    LogPage.vue added from testtest

---
+++ FrontEnd/src/views/logging/LogPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class = "information">
+        <h1>Date:</h1>
+        <h1>Type:</h1>
+
+        <div class = "btn-toolbar">
+            <button v-on:click="" type="button" class="btn btn-outline-dark user-btn btn-lg">Edit</button>
+            <button v-on:click="" type="button" class="btn btn-outline-dark reg-btn btn-lg">Delete</button>
+        </div>
+        
+    </div>    
+</template>
+
+<style>
+
+    .information {
+    display: inline-block;
+    width: 550px;
+    margin: 20px 40px;
+    text-align: left;
+    }
+
+</style>
