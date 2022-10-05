21aebf94d782d0717c02f5e2202eb1b1b85c6032
abiyaimmaculate10 <111777407+abiyaimmaculate10@users.noreply.github.com>
2022-10-05 11:43:18 +0800

    image adding function

--- FrontEnd/src/views/logging/EnterLog.vue
+++ FrontEnd/src/views/logging/EnterLog.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Footer from "../../components/Footer.vue";
 </script>
+
 <template>
     <div class="enter-log-view">
       <RouterLink to="/log"><button type="button" class="log-link" href="Log.vue">Back</button></RouterLink>
@@ -28,38 +29,87 @@
     </div>
     <!-- <input type="file" accept="image/*" @change="uploadImage($event)" id="file-input"> -->
     <!-- code to allow user to upload image -->
-  
+    <!-- <v-file-input
+      v-model="file"
+      chips
+      accept="image/*"
+      label="Image"
+    /> -->
+    <input type="file" accept="image/*" @change="onChange" />
+    <div id="preview">
+      <img v-if="item.imageUrl" :src="item.imageUrl" />
+    </div>
+
+
 </template>
   
-<script>
+<!-- <script>
 methods: {
-  //method to upload image, still figuring it out
-  // uploadImage(event) {
+  method to upload image, still figuring it out
+  uploadImage(event) {
 
-  //   const URL = 'http://foobar.com/upload';
+    const URL = 'http://foobar.com/upload';
 
-  //   let data = new FormData();
-  //   data.append('name', 'my-picture');
-  //   data.append('file', event.target.files[0]);
+    let data = new FormData();
+    data.append('name', 'my-picture');
+    data.append('file', event.target.files[0]);
 
-  //   let config = {
-  //     header: {
-  //       'Content-Type': 'image/png'
-  //     }
-  //   }
+    let config = {
+      header: {
+        'Content-Type': 'image/png'
+      }
+    }
 
-  //   axios.put(
-  //     URL,
-  //     data,
-  //     config
-  //   ).then(
-  //     response => {
-  //       console.log('image upload response > ', response)
-  //     }
-  //   )
-  // }
+    axios.put(
+      URL,
+      data,
+      config
+    ).then(
+      response => {
+        console.log('image upload response > ', response)
+      }
+    )
+  }
 }
-</script>
+</script> -->
+
+<!-- <script>
+  export default{
+    data:{file: null, image:null},
+    methods: {
+      onFileChange(){
+        const reader= new FileReader()
+        reader.readAsDataURL(this.file)
+        reader.onoload = e => {
+          this.image = e.target.result
+          console.log(this.image)
+        }
+      }
+    }
+
+  }
+</script> -->
+<script>
+  export default {
+    name: 'imageUpload',
+    data() {
+      return {
+        item:{
+            //...
+            image : null,
+            imageUrl: null
+        }
+      }
+    },
+    methods: {
+      onChange(e) {
+        const file = e.target.files[0]
+        this.image = file
+        this.item.imageUrl = URL.createObjectURL(file)
+      }
+    }
+  } 
+  </script>
 
 <style>
   h1{
