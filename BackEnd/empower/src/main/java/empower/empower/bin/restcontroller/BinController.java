a8c0806e48e4c1e85f186ba2dc12b91452c6c6f1
sohzhiyi <zhiyi.soh.2021@scis.smu.edu.sg>
2022-10-31 11:52:59 +0800

    transfer business logic from controller to service

--- BackEnd/empower/src/main/java/empower/empower/bin/restcontroller/BinController.java
+++ BackEnd/empower/src/main/java/empower/empower/bin/restcontroller/BinController.java
@@ -86,59 +86,14 @@ public class BinController {
                 binlist = binService.listBins();
         }
         //System.out.println(binlist.size() + "Longitude and Latitude: " + longitude + latitude);
-        Bin closestBin = compareCoordinates(binlist, longitude, latitude);
+        Bin closestBin = binService.compareCoordinates(binlist, longitude, latitude);
         return closestBin.getId();
     }
 
     //returns the nearest bin based on coordinate distance
-    public Bin compareCoordinates(List<Bin> list, double longitude, double latitude){
-        Bin nearest = list.get(0);
+    
 
-        System.out.println("list size is " + list.size());
-        double nearestDistance = Double.MAX_VALUE;
-        //int count = 0;
-        for (int i = 0; i < list.size(); i++) {
-            Bin currBin = list.get(i);
-            double currBinLon = currBin.getLongitude();
-            double currBinLat = currBin.getLatitude();
-            double distance = distance(currBinLat, latitude, currBinLon, longitude);
-            //System.out.println("distance calculated is " + distance);
-            if(nearestDistance > distance){
-                nearestDistance = distance;
-                nearest = currBin;
-            }
-            //System.out.println("nearest distance is " + nearestDistance);
-            //System.out.println(++count);
-        }
-        return nearest;
-    }
-
-    public static double distance(double lat1, double lat2, double lon1, double lon2) {
- 
-        // The math module contains a function
-        // named toRadians which converts from
-        // degrees to radians.
-        lon1 = Math.toRadians(lon1);
-        lon2 = Math.toRadians(lon2);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
- 
-        // Haversine formula
-        double dlon = lon2 - lon1;
-        double dlat = lat2 - lat1;
-        double a = Math.pow(Math.sin(dlat / 2), 2)
-                 + Math.cos(lat1) * Math.cos(lat2)
-                 * Math.pow(Math.sin(dlon / 2),2);
-             
-        double c = 2 * Math.asin(Math.sqrt(a));
- 
-        // Radius of earth in kilometers. Use 3956
-        // for miles
-        double r = 6371;
- 
-        // calculate the result
-        return(c * r);
-    }
+    
 
     //updates information about a bin through the bin's id
     //if the bin cannot be found by the id entered then a BinNotFoundException is thrown
