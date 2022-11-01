95cec0c37561a2c8e882c1aa53ffb1d13232ee04
sohzhiyi <zhiyi.soh.2021@scis.smu.edu.sg>
2022-11-01 14:37:23 +0800

    Add data

--- BackEnd/empower/src/main/java/empower/empower/log/service/EmissionsService.java
+++ BackEnd/empower/src/main/java/empower/empower/log/service/EmissionsService.java
@@ -1,5 +1,20 @@
 package empower.empower.log.service;
+import empower.empower.log.entity.Emissions;
+import empower.empower.log.repository.EmissionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import javax.transaction.Transactional;
 
+@Service
+@Transactional
 public class EmissionsService {
+    @Autowired
+    private EmissionsRepository emRepository;
+    
+    public List<Emissions> listAllEmissions(){
+        return emRepository.findAll();
+    }
+
     
 }
