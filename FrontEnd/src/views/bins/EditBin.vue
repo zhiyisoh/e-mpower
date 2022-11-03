328e9b667e3ff38fe1c33c594fff51889fb700f3
abiyaimmaculate10 <111777407+abiyaimmaculate10@users.noreply.github.com>
2022-11-03 17:50:12 +0800

    changed the labelling

--- FrontEnd/src/views/bins/EditBin.vue
+++ FrontEnd/src/views/bins/EditBin.vue
@@ -4,56 +4,53 @@
 </script>
 
 <template>
-    <div class="container">
-        <p>Edit the details of the address below.</p>
-    </div>
+    <div class="enter">
+        <h1>Hello Admin, made a mistake somewhere?</h1>
+            <img src="/src/assets/recycle-bin.gif" alt="leaves" class="bin-icon">
 
-    <div class="formBin">
-        <Form @submit="onSubmit" :validation-schema="schema" v-on:submit.prevent="submitForm">
-            <div class="form-group">
-                <label for="binAddress">Enter New Address</label>
-                <input name="binAddress" v-model="binAddress" type="text" class="form-control" />
+        <div class="formBin">
+            <Form @submit="onSubmit" :validation-schema="schema" v-on:submit.prevent="submitForm">
+                <div class="form-group">
+                    <label for="binAddress">Enter New Address</label>
+                    <input name="binAddress" v-model="binAddress" type="text" class="form-control" />
 
-                <label for="postalCode">Enter New Postal code</label>
-                <input name="postalCode" v-model="postalCode" type="text" class="form-control" />
+                    <label for="postalCode">Enter New Postal code</label>
+                    <input name="postalCode" v-model="postalCode" type="text" class="form-control" />
 
-                <label for="ICTRecycling">Recycles ICT?</label>
-                <input name="ICTRecycling" v-model="ICTRecycling" type="radio" id="one" value="One"  />
-                <label for="one">True</label>
+                    <label for="ICTRecycling">Recycles ICT?</label>
+                    <input name="ICTRecycling" v-model="ICTRecycling" type="radio" id="one" value="One"  />
+                    <label for="one">True</label>
 
-                <input name="ICTRecycling" v-model="ICTRecycling" type="radio" id="two" value="Two" />
-                <label for="two">False</label>
-                <br>
+                    <input name="ICTRecycling" v-model="ICTRecycling" type="radio" id="two" value="Two" />
+                    <label for="two">False</label>
+                    <br>
 
-                <label for="batteryRecycling">Recycles Battery?</label>
-                <input name="batteryRecycling" v-model="batteryRecycling" type="radio" id="one" value="One"  />
-                <label for="one">True</label>
+                    <label for="batteryRecycling">Recycles Battery?</label>
+                    <input name="batteryRecycling" v-model="batteryRecycling" type="radio" id="one" value="One"  />
+                    <label for="one">True</label>
 
-                <input name="batteryRecycling" v-model="batteryRecycling" type="radio" id="two" value="Two" />
-                <label for="two">False</label>
-                <br>
+                    <input name="batteryRecycling" v-model="batteryRecycling" type="radio" id="two" value="Two" />
+                    <label for="two">False</label>
+                    <br>
 
-                <label for="bulbRecycling">Recycles Bulbs?</label>
-                <input name="bulbRecycling" v-model="bulbRecycling" type="radio" id="one" value="One"  />
-                <label for="one">True</label>
+                    <label for="bulbRecycling">Recycles Bulbs?</label>
+                    <input name="bulbRecycling" v-model="bulbRecycling" type="radio" id="one" value="One"  />
+                    <label for="one">True</label>
 
-                <input name="bulbRecycling" v-model="bulbRecycling" type="radio" id="two" value="Two" />
-                <label for="two">False</label>
-            </div>
+                    <input name="bulbRecycling" v-model="bulbRecycling" type="radio" id="two" value="Two" />
+                    <label for="two">False</label>
+                </div>
 
-            <div class="form-group">
-                <button class="btn btn-primary btn-block editbin-submit" :disabled="loading">
-                    <span v-show="loading" class="spinner-border spinner-border-sm"></span>
-                    Submit
-                </button>
-            </div>
-        </Form>
-
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block editbin-submit" :disabled="loading">
+                        <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+                        Submit
+                    </button>
+                </div>
+            </Form>
+        </div>
         <Footer/>
     </div>
-
-
-    
 
 </template>
 
